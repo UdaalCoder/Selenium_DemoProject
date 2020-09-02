355b9149c42cd2986a44e478f02a8601d888de96
vidyagowda159 <61838885+vidyagowda159@users.noreply.github.com>
2020-09-02 17:03:48 +0530

    Till window handling

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
 from selenium import webdriver
 driver = webdriver.Chrome(executable_path=r'C:\Users\vidya gowda\PycharmProjects\Selenium_DemoProject\drivers\chromedriver.exe')
-url = ['https://demo.actitime.com/login.do']
+url = ['https://demo.actitime.com/login.do',"https://www.flipkart.com/","https://www.makemytrip.com/"]
